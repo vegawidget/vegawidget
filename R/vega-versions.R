8f31b5f7a0e626539af8208071cdb7ff74eabef5
Ian Lyttle <ian.lyttle@schneider-electric.com>
2018-06-17 13:48:41 -0500

    adds internal function `vega_versions()` to determine which JS libraries to use

---
+++ R/vega-versions.R
@@ -0,0 +1,48 @@
+#' Get versions of Vega JS libraries
+#'
+#' This is an internal function for updating this package.
+#'
+#' @param vega_lite_version `character` version of Vega-Lite, e.g. `"2.5.0"`
+#'
+#' @return `list` with elements `vega_lite`, `vega`, `vega_embed`
+#' @examples
+#'   vega_versions
+#'
+vega_versions <- function(vega_lite_version) {
+
+  if (!requireNamespace("glue", quietly = TRUE)) {
+    stop("Package \"glue\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+
+  if (!requireNamespace("httr", quietly = TRUE)) {
+    stop("Package \"httr\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+
+  url <-
+    glue::glue("https://cdn.jsdelivr.net/npm/vega-lite@{vega_lite_version}/package.json")
+
+  # get and validate response
+  resp <- httr::GET(url)
+  resp <- httr::stop_for_status(
+    resp,
+    task = paste("retrieve Vega-Lite manifest at", url, " - please verify")
+  )
+
+  # parse response
+  text <- httr::content(resp, as = "text")
+  package <- jsonlite::fromJSON(text)
+
+  # get versions
+  vega_version <- sub("\\^", "", package$devDependencies$vega)
+  vega_embed_version <- sub("\\^", "", package$devDependencies$`vega-embed`)
+
+  vega_versions <- list(
+    vega_lite = vega_lite_version,
+    vega = vega_version,
+    vega_embed = vega_embed_version
+  )
+
+  vega_versions
+}

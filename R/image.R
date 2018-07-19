4f500a22fce46d716b4a6531aaf76ac87f5ad5b8
Ian Lyttle <ian.lyttle@schneider-electric.com>
2018-07-19 13:06:17 -0500

    first pass at write_png(), #18

--- R/image.R
+++ R/image.R
@@ -1,3 +1,32 @@
+write_png <- function(spec, path, width = NULL, height = NULL) {
+
+  # this inspired by the approach taken by vegalite:
+  #  https://github.com/hrbrmstr/vegalite/blob/cdf7ee9c90bb71661ba00e20e7f6575acbf2e1c8/R/phantom.r
+
+  # create a copy of the spec
+  spec_new <- autosize(spec, width = width, height = height)
+
+  # create a widget
+  widget <-
+    vegawidget(
+      spec_new,
+      embed = vega_embed(renderer = "canvas"), # just to be sure
+      width = width,
+      height = height
+    )
+
+  # write widget to an html file
+  file_widget_html <- tempfile(fileext = ".html")
+  url_widget_html <- glue::glue("file://{file_widget_html}")
+  htmlwidgets::saveWidget(widget, file = file_widget_html)
+  # print(url_widget_html)
+
+  # use webshot to take a picture
+  webshot::webshot(url_widget_html, file = path, selector = "canvas.marks")
+
+  # return original spec
+  spec
+}
 
 # this does *NOT* work
 to_svg <- function(spec, scale = 1) {
@@ -38,9 +67,6 @@ to_svg <- function(spec, scale = 1) {
 
 }
 
-write_png <- function(spec) {
-
-}
 
 
 

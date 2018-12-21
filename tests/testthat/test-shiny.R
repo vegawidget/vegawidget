5625b2a51e6c5d5103ba3114a823b012e99f6fd3
Ian Lyttle <ian.lyttle@schneider-electric.com>
2018-12-21 10:12:54 -0600

    experiment to see if shiny-test might still work on travis

--- tests/testthat/test-shiny.R
+++ tests/testthat/test-shiny.R
@@ -5,7 +5,7 @@ library(shinytest)
 test_that("binding ui to signal and listening works", {
   # Don't run these tests on the CRAN build servers
   skip_on_cran()
-  skip_on_travis()
+  #skip_on_travis()
   # for now, the test does not work on my (Ian's) MacBook,
   # so I cannot update, so tests are destined to fail
 

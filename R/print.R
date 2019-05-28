74d672a67dddb1226676e45c87c864a89dcc6a1a
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-05-28 17:54:34 -0500

    Update knit_print() to handle svg and pdf (as well as png)

--- R/print.R
+++ R/print.R
@@ -66,37 +66,68 @@ knit_print.vegaspec <- function(spec, ..., options = NULL){
   height <- to_int(options$vega.height)
 
   fmt <- knitr::opts_knit$get("rmarkdown.pandoc.to")
+
+  # if this goes to HTML, print and be done!
   if (knitr::is_html_output(fmt)) {
-    knitr::knit_print(
-      vegawidget(spec, embed = embed, width = width, height = height)
+    return(
+      knitr::knit_print(
+        vegawidget(spec, embed = embed, width = width, height = height)
+      )
     )
-  } else {
-    # knitr's default screenshoting won't work due to incompatibility with
-    # es6 JS code and webshot. Thus interecept here and do the conversion to
-    # static image ourselves...
-    tryCatch({
+  }
+
+  # this does not go to html; we have more thinking to do...
+
+  # knitr's default screenshoting won't work due to incompatibility with
+  # es6 JS code and webshot. Thus interecept here and do the conversion to
+  # static image ourselves...
+
+  # I don't know that we need this here, because knitr seems very good about
+  # always supplying a value for `dev`.
+  dev_default <- "svg"
+  if (knitr::is_latex_output()) {
+    dev_default <- "pdf"
+  }
+
+  dev <- options$dev %||% knitr::opts_chunk$get("dev") %||% dev_default
+
+  # choose writing-function
+  fn_write <- switch(
+    dev,
+    png = vw_write_png,
+    svg = vw_write_svg,
+    pdf = {
+      assert_packages("rsvg")
+      function(spec, path, ...) {
+        svg <- vw_to_svg(spec, ...)
+        rsvg::rsvg_pdf(charToRaw(svg), path)
+      }
+    },
+    # default, if dev not recognized
+    function(spec, path, ...) {
+      stop("unknown device type: `", dev, "`.")
+    }
+  )
+
+  tryCatch({
       f <- tempfile()
       on.exit({unlink(f)})
-      knitr::opts_chunk$set(out.width = 100)
-      vw_write_png(spec, path = f, width = width, height = height)
+      fn_write(spec, path = f, width = width, height = height)
       res <- readBin(f, "raw", file.info(f)[, "size"])
       structure(
-        list(image = res, extension = ".png"),
+        list(image = res, extension = paste0(".", dev)),
         class = "html_screenshot"
       )
-    }, error = function(e) {
-      err_msg <- c("Error printing vegawidget in non-HTML format:",
-                       conditionMessage(e))
-      knitr::knit_print(err_msg)}
-    )
-  }
+    },
+    error = function(e) {
+      err_msg <- c(
+        "Error printing vegawidget in non-HTML format:",
+        conditionMessage(e)
+      )
+      knitr::knit_print(err_msg)
+    }
+  )
+
 }
 
-# tells us if we are knitting or not (knot?)
-is_knit <- function() {
-  if (!requireNamespace("knitr", quietly = TRUE)) {
-    return(FALSE)
-  }
 
-  !is.null(knitr::opts_knit$get("rmarkdown.pandoc.to"))
-}

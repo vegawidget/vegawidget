c64d98c464fc22ba43623254a0a0eca443b49348
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-06-23 08:04:43 -0500

    Remove debudding code

--- data-raw/templates/vegawidget.js
+++ data-raw/templates/vegawidget.js
@@ -45,15 +45,10 @@ HTMLWidgets.widget({
       // x, object to instantitate htmlwidget
       renderValue: function(x) {
 
-        console.log(x.embed_options.loader);
-
         // if x has base_url use it
         if (x.base_url !== null && typeof x.base_url !== 'undefined'){
           x.embed_options.loader = vega.loader({baseURL: x.base_url});
         }
-
-        console.log(x.base_url);
-        console.log(x.embed_options.loader);
 
         // initialise promise
         view_promise =

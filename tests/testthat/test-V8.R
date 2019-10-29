534e9cee549e9965b7840c532ac9ab603741a6ad
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-10-29 12:03:24 -0500

    test V8 ES6

--- tests/testthat/test-V8.R
+++ tests/testthat/test-V8.R
@@ -1,3 +1,13 @@
+library("V8")
+
+ctx <- v8()
+
+test_that("V8 (ES5) works", {
+  ctx$eval("var foo = 123")
+  expect_identical(ctx$get("foo"), 123L)
+})
+
 test_that("V8 (ES6) works", {
-  expect_equal(2 * 2, 4)
+  ctx$eval("let bar = 123")
+  expect_identical(ctx$get("bar"), 123L)
 })

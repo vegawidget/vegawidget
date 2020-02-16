fedc3d91d6f8f6afd2e0f2cf3d5f22fb55268f62
Ian Lyttle <ijlyttle@users.noreply.github.com>
2020-02-16 07:28:39 -0600

    Embed actions (#118)
    * bump version
    
    * refactor vega_embed() to consider actions$export as a list

--- tests/testthat/test-vega-embed.R
+++ tests/testthat/test-vega-embed.R
@@ -6,9 +6,13 @@ test_that("validation works", {
   expect_error(vega_embed(renderer = "foo"))
 
   # actions
-  expect_error(vega_embed(actions = list(TRUE, TRUE)), "must have length 1")
-  expect_error(vega_embed(actions = list(source = TRUE, FALSE)), "must have length 1")
-  expect_error(vega_embed(actions = list(foo = TRUE)), "illegal name")
+  expect_error(vega_embed(actions = list(TRUE, TRUE)), "named")
+  expect_error(vega_embed(actions = list(source = TRUE, FALSE)), "legal name")
+  expect_error(vega_embed(actions = list(foo = TRUE)), "legal name")
+
+  # actions$export
+  expect_error(vega_embed(actions = list(export = list(TRUE, TRUE))), "named")
+  expect_error(vega_embed(actions = list(export = list(foo = TRUE))), "legal name")
 
 })
 
@@ -22,6 +26,18 @@ test_that("constructor works", {
 
   expect_identical(test_embed$renderer, "svg")
   expect_identical(test_embed$actions, list(source = FALSE))
+
+  # test export
+  test_embed_export <-
+    vega_embed(
+      renderer = "svg",
+      actions = list(export = list(svg = TRUE, png = FALSE))
+    )
+
+  expect_identical(
+    test_embed_export$actions$export,
+    list(svg = TRUE, png = FALSE)
+  )
 
 })
 

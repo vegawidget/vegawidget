77261198234bfac88e472d3ab37c75370c906ff3
Ian Lyttle <ian.lyttle@se.com>
2021-06-27 21:03:13 -0500

    adjust test for windows

--- tests/testthat/test-callback-helpers.R
+++ tests/testthat/test-callback-helpers.R
@@ -5,6 +5,7 @@ ct <- V8::v8()
 ct$source(system.file("bin", "vega_to_svg_v8.js", package = "vegawidget"))
 
 test_that("vw_fetch() works", {
+
   expect_snapshot(
     vw_fetch("https://vega.github.io/vega-datasets/data/anscombe.json")
   )
@@ -19,6 +20,11 @@ test_that("vw_fetch() works", {
 test_that("vw_load() works", {
 
   tmpfile <- local_tempfile()
+
+  # get rid of any backslashes (Windows)
+  # we will have to keep that in mind in vw_to_svg()
+  tmpfile <- gsub("\\\\", "/", tmpfile)
+
   writeLines("foobar", tmpfile)
 
   expect_snapshot(

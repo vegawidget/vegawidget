03842199516a4e13cbc8237fff0e8c3e526bae7e
Ian Lyttle <ian.lyttle@se.com>
2023-09-02 15:06:44 -0500

    rework versions

--- R/vega-version.R
+++ R/vega-version.R
@@ -147,8 +147,12 @@ get_major <- function(x) {
 get_candidate <- function(version, candidates) {
 
   # package_version needs to be vectorized
+  package_major <- function(x) {
+    package_version(x)$major
+  }
+
   major <- function(x) {
-    vapply(x, function(x) package_version(x)$major, numeric(1), USE.NAMES = FALSE)
+    vapply(x, package_major, numeric(1), USE.NAMES = FALSE)
   }
 
   # need to save original for message
@@ -158,10 +162,6 @@ get_candidate <- function(version, candidates) {
   if (!grepl("\\.", version)) {
     version <- glue::glue("{version}.0")
   }
-
-  # cast as numeric versions
-  version <- numeric_version(version)
-  candidates <- numeric_version(candidates)
 
   # if version smaller than smallest candidate, use smallest candidate
   if (major(version) < min(major(candidates))) {
